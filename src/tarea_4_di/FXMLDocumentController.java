ebc7bcbd9f4c23cffd35be8146f0dd319a1d1fe4
Alejandro Martínez <amj.parrales@gmail.com>
2024-12-10 13:32:35 +0100

    Boton mostrar con filtrado implementado, comienzo a configurar el informe a su vez

--- src/tarea_4_di/FXMLDocumentController.java
+++ src/tarea_4_di/FXMLDocumentController.java
@@ -1,6 +1,8 @@
 package tarea_4_di;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.application.Platform;
@@ -8,8 +10,12 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.fxutils.viewer.JasperViewerFX;
 
 public class FXMLDocumentController implements Initializable {
     
@@ -40,17 +46,46 @@ public class FXMLDocumentController implements Initializable {
         GestionAlumnos gestion = new GestionAlumnos();
         List <Alumnos> datos = gestion.datosInforme();
         
-        //Muestro primero los datos por consola.
-        System.out.println("Infome de alumnos: ");
-        
         for(Alumnos alumno : datos  ){
-            System.out.println(
+            
+             // Crear una lista para almacenar solo los alumnos de "DI".
+             List<Alumnos> alumnosDI = new ArrayList<>();
+            
+            if("DI".equals(alumno.getSiglasDeModulo())){
+                alumnosDI.add(alumno);
+                 //Muestro primero los datos por consola.
+                System.out.println("Infome de alumnos: ");
+                System.out.println(
                 "DNI: " + alumno.getDni() + "\t"+
                 "Modulo: " + alumno.getSiglasDeModulo() +"\t"+
                 "Nota del modulo: " + alumno.getNotaModulo() +"\t"+
                 "Provincia: " + alumno.getProvincia());
+            }
         }
+        try{
+             //Defino la colección de datos sobre la que se crea el informe.
+        JRBeanCollectionDataSource beanColDataSource = new JRBeanCollectionDataSource(datos);
         
+        Node source = (Node) event.getSource();
+        
+        Stage stage = (Stage) source.getScene().getWindow();
+        
+         JasperViewerFX viewerfx = new JasperViewerFX(
+            stage,
+            "Informe de Alumnos",
+            "/informes/tablaAlumnos.jasper",
+            new HashMap<>(),
+            beanColDataSource
+        );
+        
+        viewerfx = new JasperViewerFX (stage, "Informe","/informes/tablaAlumnos.jasper", new HashMap(), beanColDataSource);
+        
+        viewerfx.show();
+        
+        }catch (Exception ex){
+            System.out.println("Error." + ex.getMessage());
+        }
+       
     }
 
     @Override
--- src/tarea_4_di/GestionAlumnos.java
+++ src/tarea_4_di/GestionAlumnos.java
@@ -54,5 +54,4 @@ public class GestionAlumnos {
         return crearAlumno();
     }
     
-
 }
